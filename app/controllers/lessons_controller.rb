4f0b18664a8119a8fd93f311d802ca0e1a51441f
Teresa McClintic <tmcclintic@gmail.com>
2018-03-12 01:55:52 +0000

    add payment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_currently_enrolled, only: [:show]
   def show
   end
 
@@ -7,6 +9,13 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+ 
+  def require_currently_enrolled
+    if ! current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), :alert => "You must be enrolled in class to view the lesson"
+    end
   end
   
 end
